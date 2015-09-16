bcb59f50d10fdff2b0ea597edbc1884c7ee5b919
theilgaard <kristian.theilgaard@gmail.com>
2015-09-16 20:46:06 +0200

    Adds missing file. Prettifies.

--- SirRaspy.js
+++ SirRaspy.js
@@ -6,8 +6,9 @@ var ccontrol = require('./modules/channel-control.js');
 var ircConfig = config.get('ircConfig');
 
 var client = new irc.Client(config.ircConfig.server,
-                            config.ircConfig.botname,
-                            { channels: [ config.ircConfig.channel ] }
+  config.ircConfig.botname, {
+    channels: [config.ircConfig.channel]
+  }
 );
 
 // bootstrap google module and inject bot client
@@ -15,29 +16,36 @@ google(client);
 ccontrol(client);
 
 client.addListener('join', function(channel, who) {
-    console.log('%s has joined %s', who, channel);
+  console.log('%s has joined %s', who, channel);
 });
 
 client.addListener('error', function(message) {
-    console.log('error: ', message);
+  console.log('error: ', message);
 });
 
-client.addListener('message', function (from, to, message) {
-    console.log('%s => %s: %s', from, to, message);
+client.addListener('message', function(from, to, message) {
+  console.log('%s => %s: %s', from, to, message);
 
-    if ( to.match(/^[#&]/) ) {
-        // channel message
-        if ( message.match(/hello/i) ) {
-            client.say(to, 'Hello there ' + from);
-        }
-        if ( message.match(/dance/) ) {
-            setTimeout(function () { client.say(to, "\u0001ACTION dances: :D\\-<\u0001") }, 1000);
-            setTimeout(function () { client.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 2000);
-            setTimeout(function () { client.say(to, "\u0001ACTION dances: :D/-<\u0001")  }, 3000);
-            setTimeout(function () { client.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 4000);
-        }
+  if (to.match(/^[#&]/)) {
+    // channel message
+    if (message.match(/hello/i)) {
+      client.say(to, 'Hello there ' + from);
     }
-    else {
-        // private message
+    if (message.match(/dance/)) {
+      setTimeout(function() {
+        client.say(to, "\u0001ACTION dances: :D\\-<\u0001")
+      }, 1000);
+      setTimeout(function() {
+        client.say(to, "\u0001ACTION dances: :D|-<\u0001")
+      }, 2000);
+      setTimeout(function() {
+        client.say(to, "\u0001ACTION dances: :D/-<\u0001")
+      }, 3000);
+      setTimeout(function() {
+        client.say(to, "\u0001ACTION dances: :D|-<\u0001")
+      }, 4000);
     }
+  } else {
+    // private message
+  }
 });
---
+++ modules/channel-control.js
@@ -0,0 +1,29 @@
+(function() {
+  module.exports = function(client) {
+    var commands = [
+      ["can haz op?", 'client.send("MODE", to, "+o", from)', 'Get op on channel']
+      ["halp", 'ccontrol_printHelp(to)', 'Print this help message']
+    ];
+    keyphrases = [
+      ["Does the bear shit in the woods?", "yes"]
+    ];
+
+    client.addListener('message', function(from, to, message) {
+      for (var i = 0; i < commands.length; i++) {
+        if (message.match(commands[i][0])) {
+          console.log('Command matching: ' + commands[i][0])
+          eval(commands[i][1]);
+        } else {
+          console.log("No match for: " + message)
+        }
+      }
+    });
+  };
+})();
+
+ccontrol_printHelp = function(to) {
+  client.say(to, '[+] Channel control v1.0');
+  for (var i = commands.length - 1; i >= 0; i--) {
+    client.say(to, commands[i][0] + " : " + commands[i][2]);
+  };
+}
